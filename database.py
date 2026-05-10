51a87a53621ab4920f586f3f9152ac9053eb1259
Wallzray <wallzraymond@gmail.com>
2026-05-10 20:41:07 +0300

    Db updated

--- database.py
+++ database.py
@@ -4,15 +4,23 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+raw_uri = os.getenv("DATABASE_URL")
+
+if raw_uri:
+    # 1. Standard Render Fix
+    if raw_uri.startswith("postgres://"):
+        raw_uri = raw_uri.replace("postgres://", "postgresql://", 1)
     
-if "postgresql" in SQLALCHEMY_DATABASE_URL and "+psycopg2" not in SQLALCHEMY_DATABASE_URL:
-        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://")
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+    # 2. Force SQLAlchemy to use the pg8000 driver
+    # Note the +pg8000 in the string
+    if "postgresql" in raw_uri:
+        SQLALCHEMY_DATABASE_URL = raw_uri.replace("postgresql://", "postgresql+psycopg2://")
+    else:
+        SQLALCHEMY_DATABASE_URL = raw_uri
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,

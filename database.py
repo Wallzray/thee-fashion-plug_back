698e0013e4861cbbde19eb458431d96b52b74b20
Wallzray <wallzraymond@gmail.com>
2026-05-10 20:10:56 +0300

    db updates

--- database.py
+++ database.py
@@ -7,7 +7,9 @@ from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
+    
+if "postgresql" in SQLALCHEMY_DATABASE_URL and "+psycopg2" not in SQLALCHEMY_DATABASE_URL:
+        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://")
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
